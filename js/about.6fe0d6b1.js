0bfbb0efbb7182ebb5e6a2a9eea8f8ef32bcadbb
Bernd <dev69@mailbox.org>
2020-05-03 12:42:15 +0200

    Deploy

--- js/about.6fe0d6b1.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{"84ba":function(n,e,a){"use strict";a.r(e);var t=function(){var n=this,e=n.$createElement,a=n._self._c||e;return a("v-container",[a("h1",[n._v("Einkaufsliste")]),a("h1",[n._v("Familie Ahrens")])])},i=[],l=a("2877"),r=a("6544"),s=a.n(r),u=a("a523"),o={},c=Object(l["a"])(o,t,i,!1,null,null,null);e["default"]=c.exports;s()(c,{VContainer:u["a"]})}}]);
-//# sourceMappingURL=about.6fe0d6b1.js.map
